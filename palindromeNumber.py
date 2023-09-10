4af436924d22a6f787309f082d14c666f3bd957c
Ishwor <ishwortimalsina39@gmail.com>
2023-09-10 22:27:42 +0545

    modified

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -20,42 +20,42 @@
 # Output: false
 # Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
+# class Solution:
+#     def isPalindrome(self,x:int)->bool:
+#         # if x < 0:
+#         #     return False
+#         # original = x
+#         # reverse = 0
+#         # while x > 0:
+#         #     digit = x % 10;
+#         #     reverse = reverse * 10 + digit
+#         #     x //= 10
+#         # if reverse == original:
+#         #     return True
 class Solution:
-    def isPalindrome(self,x:int)->bool:
-        # if x < 0:
-        #     return False
-        # original = x
-        # reverse = 0
-        # while x > 0:
-        #     digit = x % 10;
-        #     reverse = reverse * 10 + digit
-        #     x //= 10
-        # if reverse == original:
-        #     return True
-
-
+    def isPalindrome(self, x: int) -> bool:
         # Handle negative numbers
         if x < 0:
             return False
-        
+
         # Convert the integer to a string
         x_str = str(x)
         
-        # Check if the string is the same as its reverse
-        return x_str == x_str[::-1]
+        # Get the length of the string
+        n = len(x_str)
+        
+        # Iterate through the first half of the string
+        for i in range(n // 2):
+            # Compare characters from the beginning and end of the string
+            if x_str[i] != x_str[n - i - 1]:
+                return False
+        
+        # If no non-matching characters were found, it's a palindrome
+        return True
 
 # Test cases
 if __name__ == "__main__":
     solution = Solution()
-    
-    # Test case 1: Palindrome number
-    result1 = solution.isPalindrome(121)
-    print(f"121 is a palindrome: {result1}")  # Expected output: True
-    
-    # Test case 2: Non-palindrome number
-    result2 = solution.isPalindrome(-121)
-    print(f"-121 is a palindrome: {result2}")  # Expected output: False
-    
-    # Test case 3: Single-digit number
-    result3 = solution.isPalindrome(10)
-    print(f"5 is a palindrome: {result3}")  # Expected output: False
+    print(solution.isPalindrome(121))  # True (121 is a palindrome)
+    print(solution.isPalindrome(-121)) # False (negative numbers can't be palindromes)
+    print(solution.isPalindrome(10))   # False (10 is not a palindrome)
