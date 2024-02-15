9c46874bbfcb9c4c096b5bd689a3ef7a4471c577
Ishwor <ishwortimalsina39@gmail.com>
2024-02-15 18:43:14 +0545

    new commit

--- maxProfit.py
+++ maxProfit.py
@@ -18,7 +18,7 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy = float(inf)
+        buy = float('inf')
         max_profit = 0
 
         for price in prices:
