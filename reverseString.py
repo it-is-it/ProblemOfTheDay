0dd5de5f51b3de3a5ec75802ef6ad67bf0115190
Ishwor <ishwortimalsina39@gmail.com>
2025-01-19 22:53:23 +0545

    problem

--- reverseString.py
+++ reverseString.py
@@ -9,3 +9,15 @@
 # Example 2:
 # Input: s = ["H","a","n","n","a","h"]
 # Output: ["h","a","n","n","a","H"]
+
+from typing import List
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
